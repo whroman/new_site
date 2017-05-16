cf8c8ccbadff5ddccd74bd142193ae5a21773964
whroman <me@walterroman.com>
2017-05-16 23:32:11 +0200

    better js loop

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 document.getElementById("project-header").addEventListener("click", function (event) {
     var toShow = document.querySelectorAll(".projects-list");
-    for (var iter = 0; iter < toShow.length; iter++) {
+    var iter = toShow.length;
+    while (iter--) {
         toShow[iter].classList.toggle("hide");
     }
     event.preventDefault();
