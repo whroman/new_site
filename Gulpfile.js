d000aca36175a6aa25a06b36e714e2bc56d78e1f
whroman <me@walterroman.com>
2017-05-17 00:20:00 +0200

    moved template data to own module

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp')
+var data = require('./src/data')
 
 // Loads all gulp plugins located in package.json
 // > Call plugins using `gp.<camelizedPluginName>
@@ -24,56 +25,6 @@ gulp.task('build-styles', () =>
         .on('error', gp.util.log)
         .pipe(gulp.dest(paths.build))
 )
-
-const createLink = (name, url) => ({ name, url })
-
-const createProject = (name, url, techs) => ({ name, url, techs })
-
-
-const data = {
-    links: [
-        createLink('stackoverflow', 'http://stackoverflow.com/users/2539700/walter-roman'),
-        createLink('instagram', 'http://instagram.com/ralterwoman'),
-        createLink('github', 'https://github.com/whroman'),
-    ],
-    projects: [
-        [
-            createProject('babbel', 'https://www.babbel.com', [
-                createLink('react-redux', 'https://github.com/reactjs/react-redux'),
-                createLink('webpack', 'https://webpack.github.io'),
-                createLink('mocha', 'https://mochajs.org')
-            ]),
-            createProject('prompt.ly', 'http://www.prompt.ly', [
-                createLink('react-redux', 'https://github.com/reactjs/react-redux'),
-                createLink('angular.js', 'http://angularjs.org/'),
-                createLink('node', 'https://nodejs.org/'),
-                createLink('trigger.io', 'https://trigger.io')
-            ]),
-            createProject('minesweeper', 'https://github.com/whroman/minesweeper-react', [
-                createLink('react', 'https://facebook.github.io/react'),
-                createLink('reflux', 'https://github.com/spoike/refluxjs'),
-                createLink('istanbul', 'https://github.com/gotwarlost/istanbul')
-            ]),
-        ],
-        [
-            createProject('kiwi', 'https://kiwi.ki', [
-                createLink('backbone.js', 'http://backbonejs.org'),
-                createLink('browserify', 'http://browserify.org')
-            ]),
-            createProject('mortgage scraper', 'https://github.com/tinta/ALLegalsScraper', [
-                createLink('jasmine', 'http://jasmine.github.io/2.3/introduction.html'),
-                createLink('google oauth', 'http://passportjs.org'),
-                createLink('phantom.js', 'http://phantomjs.org')
-            ]),
-            createProject('successfactors', 'http://www.successfactors.com/en_us/customers.html', [
-                createLink('jquery', 'http://jquery.com')
-            ]),
-            createProject('gaza-graphs', 'http://whroman.github.io/gaza-graphs', [
-                createLink('d3.js', 'http://d3js.org'),
-            ]),
-        ]
-    ]
-}
 
 gulp.task('build-html', () =>
     gulp.src(paths.ejs.src)
---
+++ src/data.js
@@ -0,0 +1,46 @@
+const createLink = (name, url, techs) => ({ name, url, techs })
+
+module.exports = {
+    links: [
+        createLink('stackoverflow', 'http://stackoverflow.com/users/2539700/walter-roman'),
+        createLink('instagram', 'http://instagram.com/ralterwoman'),
+        createLink('github', 'https://github.com/whroman'),
+    ],
+    projects: [
+        [
+            createLink('babbel', 'https://www.babbel.com', [
+                createLink('react-redux', 'https://github.com/reactjs/react-redux'),
+                createLink('webpack', 'https://webpack.github.io'),
+                createLink('mocha', 'https://mochajs.org')
+            ]),
+            createLink('prompt.ly', 'http://www.prompt.ly', [
+                createLink('react-redux', 'https://github.com/reactjs/react-redux'),
+                createLink('angular.js', 'http://angularjs.org/'),
+                createLink('node', 'https://nodejs.org/'),
+                createLink('trigger.io', 'https://trigger.io')
+            ]),
+            createLink('minesweeper', 'https://github.com/whroman/minesweeper-react', [
+                createLink('react', 'https://facebook.github.io/react'),
+                createLink('reflux', 'https://github.com/spoike/refluxjs'),
+                createLink('istanbul', 'https://github.com/gotwarlost/istanbul')
+            ]),
+        ],
+        [
+            createLink('kiwi', 'https://kiwi.ki', [
+                createLink('backbone.js', 'http://backbonejs.org'),
+                createLink('browserify', 'http://browserify.org')
+            ]),
+            createLink('mortgage scraper', 'https://github.com/tinta/ALLegalsScraper', [
+                createLink('jasmine', 'http://jasmine.github.io/2.3/introduction.html'),
+                createLink('google oauth', 'http://passportjs.org'),
+                createLink('phantom.js', 'http://phantomjs.org')
+            ]),
+            createLink('successfactors', 'http://www.successfactors.com/en_us/customers.html', [
+                createLink('jquery', 'http://jquery.com')
+            ]),
+            createLink('gaza-graphs', 'http://whroman.github.io/gaza-graphs', [
+                createLink('d3.js', 'http://d3js.org'),
+            ]),
+        ]
+    ]
+}
