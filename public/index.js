713de2a08ce677677a70ee6d04dd6e6605a211a0
whroman <me@walterroman.com>
2017-05-17 00:29:47 +0200

    site is now js-free

--- public/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("project-header").addEventListener("click", function (event) {
-    var toShow = document.querySelectorAll(".projects-list");
-    var iter = toShow.length;
-    while (iter--) {
-        toShow[iter].classList.toggle("hide");
-    }
-    event.preventDefault();
-});
